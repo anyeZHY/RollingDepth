722651477898f62e2e783c7bf47e950104cc81ff
Haoyu Zhen <47559723+anyeZHY@users.noreply.github.com>
2025-06-04 14:09:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+def read_requirements(filename):
+    with open(filename) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
 setup(
     name="rollingdepth",
     version="1.0",
     packages=find_packages(),  # Automatically find all packages (my_folder, inference, etc.)
+    install_requires=read_requirements('requirements.txt'),
 )
