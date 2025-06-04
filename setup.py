3e1f9d57ab1fb8dbac20733cd8fedbf0a436afbf
Haoyu Zhen <47559723+anyeZHY@users.noreply.github.com>
2025-06-04 13:42:56 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rollingdepth",
+    version="1.0",
+    packages=find_packages(),  # Automatically find all packages (my_folder, inference, etc.)
+)
